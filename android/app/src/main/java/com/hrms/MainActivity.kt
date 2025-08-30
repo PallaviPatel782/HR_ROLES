bba2ed7335fc9d04924f1f9218de52a344204883
PallaviPatel782 <pallavipatel782@gmail.com>
2025-08-30 19:46:54 +0530

    30-8-25

--- android/app/src/main/java/com/hrms/MainActivity.kt
+++ android/app/src/main/java/com/hrms/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.hrms
+package com.trinexa.hrms
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/hrms/MainApplication.kt
+++ android/app/src/main/java/com/hrms/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.hrms
+package com.trinexa.hrms
 
 import android.app.Application
 import com.facebook.react.PackageList
